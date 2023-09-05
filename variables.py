a10efdea4c254f7fbc03149756e183a660249ef1
Scholar <scholar@paradigm.local>
2023-09-05 13:27:51 -0600

    Variables and a function

---
+++ variables.py
@@ -0,0 +1,17 @@
+my_name = "Vivienne"
+test_score = 100
+was_lunch_tasty = True 
+b_day_class_list = ['Concert band', 'Env science', 'computer programing', 'math 2']
+
+print(f"Hi, my name is {my_name}. I just scored {test_score} on my most recent test.My classes today are {b_day_class_list}.")
+
+def add_two_numbers(num1, num2):
+    print(1 +2)
+
+def add_two_numbers2(num1, num2):
+    print(num1 + num2)
+user_number1 = int(input("Give me a number: "))
+user_number2 = int(input("Give me a number "))
+
+add_two_numbers2(user_number1, user_number2)
+
