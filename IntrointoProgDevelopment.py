b14d88e45b4e08882a831932877694106d9f3b49
Scholar <scholar@paradigm.local>
2023-09-05 13:33:49 -0600

    Into to prog Development

---
+++ IntrointoProgDevelopment.py
@@ -0,0 +1,29 @@
+person = "Barrett"
+
+ # for i in range(10):
+ # print(i)
+
+list = ('one', 'two', 'three')
+
+dictionary = {
+    'name' : 'Barrett',
+    'age' : 16
+}
+
+def is_even_number(number):
+    if number % 2 == 0:
+        print('This is an even number!')
+    else:
+        print('This is an oddd number!')
+
+is_even_number(2)
+
+MAIN_CHARACTER = "Mario"
+
+main_character = "Luigi"
+
+myName = "vivienne"
+
+my_name = "bill"
+
+MyName = "Bob"
