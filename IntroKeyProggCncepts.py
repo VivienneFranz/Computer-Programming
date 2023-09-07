e134267a511d5398942256538f0312d5d4e22c3c
Scholar <scholar@paradigm.local>
2023-09-07 13:31:25 -0600

    Prog Concepts

---
+++ IntroKeyProggCncepts.py
@@ -0,0 +1,45 @@
+my_name = "Vivienne"
+
+def give_a_name():
+    a_name = "William"
+    print(a_name)
+
+print(my_name)
+
+user_input = input("what is your name? ")
+print(f'Nice to meet you, {user_input}!')
+
+my_string = "This is my string."
+my_integer = 1456
+my_float = 3.14
+is_my_boolean = True
+
+
+print(my_float + my_float)
+
+my_declaration = ""
+my_initialization = "Hello World"
+
+add = 1 + 2
+subtract = 2 - 1
+divide = 2 / 1
+multiply = 1 * 3 
+modulo = 2 % 1 
+exponent = 2 ** 3
+
+print(modulo)
+
+statement = 4 > 2 
+statement = 4 >= 2 
+statement = 4 <= 2
+statement = 4 ==2
+statement = 4 != 2 
+
+print(statement)
+
+
+message = 4 > 2 and 33 < 43
+message2 = 4 < 2 or 33 < 43
+X = false
+
+print(not X)
