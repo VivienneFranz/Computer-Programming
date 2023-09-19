e2e0e66e44fbe8bbb95a8c4f82301058a1b5b07d
Scholar <scholar@paradigm.local>
2023-09-19 13:31:46 -0600

    intro to control

---
+++ IntroControlStruct.py
@@ -0,0 +1,34 @@
+# car = "Blue"
+
+# if car == "Blue" or car == "Blue":
+#     print("My car is blue")
+# elif car == "Red":
+#     print("My car is a ferrari")
+# else:
+#     print("It just have a bike")
+
+# car_color = "Blue"
+# car_make = "Tesla"
+
+# if car_color == "Blue":
+#     print("My car is Blue")
+#     if car_make == "Tesla":
+#         print("My car is a Blue Tesla")
+# elif car_color == "Red":
+#     print('My car is a Red ferrari')
+# else:
+#     print("I just have a bike")
+
+# for loop
+# for i in range(1, 10):
+#     print(i)
+
+# sports = ['soccer', 'basketball', 'hockey', 'football']
+
+# for item in sports:
+#     print(item)
+
+i = 1
+while 1 < 6:
+    print("I'm still going")
+    i += 1
