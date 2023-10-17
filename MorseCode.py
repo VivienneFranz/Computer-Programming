8ea5c39cb375d03ee472d8fca4285f78e0dc3d64
Scholar <scholar@paradigm.local>
2023-10-17 13:08:08 -0600

    MorseCode

--- MorseCode.py
+++ MorseCode.py
@@ -1,18 +1,36 @@
 
-morse_dict = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'j': '.---', 'K': '-.-', 'L': '.-..', 'M': '--' 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----', ',': '--..--', '.': '.-.-.-', '?': '..--..', '"': '.-..-.', ':': '---...', "'": '.----.', '-': '-....-', '/': '-..-.', '(': '-.--.', ')': '-.--.-'}
+morse_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '.': '.-.-.-', ',': '--..--',
+    '?': '..--..', "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.', ')': '-.--.-',
+    ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.',
+    '$': '...-..-', '@': '.--.-.', ' ': '/'}
 
-def from_morse_code(morse_code):
-
-    message = ''
-    morse_code = morse_code.split(' ')
-    for code in morse_code:
-        for char, morse in morse_dict.items():
-            if morse == code:
-                message += char
-    return message
+def to_morse_code(message):
+    
+    morse_code = ''
+    for char in message.upper():
+        if char in morse_dict:
+            morse_code += morse_dict[char] + ' '
+    return morse_code
 
 def main():
-    morse_code_sequence = ".. / .- -- / .- / -... --- -.-- .-.-.-"
-    print("Morse code sequence: ", morse_code_sequence, "\nMessage: ", from_morse_code(morse_code_sequence))
+    while True:
+        choice = input("Choose an option:\nPress 1 to convert text to Morse code\nPress 2 to convert Morse code to text\nPress -1 to quit\n")
+        if choice == '1':
+            message = input("Enter a message to translate to Morse code: ")
+            morse_code = to_morse_code(message)
+            print(morse_code)
+        
+        elif choice == '2':
+            morse_code = input("Quitted successfully")
+            break
+        
+        else:
+            print("Erorr in choice, please choose from our options.")
 
-user_1 = int(input("What sentence do  you want to turn into morse code?"))
+if __name__ == "__main__":
+    main()
---
+++ hello.py
@@ -0,0 +1,5 @@
+i know who you are
+
+Run
+
+no you dont kellan 
