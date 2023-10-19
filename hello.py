f68fa4394709529919a815210d8357aafea2bf7a
Scholar <scholar@paradigm.local>
2023-10-19 12:48:43 -0600

    FizzBuss

--- hello.py
+++ hello.py
@@ -2,4 +2,8 @@ i know who you are
 
 Run
 
-no you dont kellan 
+no you dont kellan Shhh
+
+im better than you, i never lose
+
+hey girl are you extra long lasting flavor spearmint natural and artifically flavored 
